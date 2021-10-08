e3329f4e793efe95cf746c472a5028d957a9e7f4
PierreLeBris <pierre.lebris@outlook.com>
2021-10-08 10:39:08 +0200

    oui 10:40

--- Web_Mobile/sekai/src/App.js
+++ Web_Mobile/sekai/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header'
+import Counter from './components/counter'
 
 function App() {
   return (
     <div className="App">
+      <Counter></Counter>
       <Header label="Vive La France"/>
       <Header></Header>
-
     </div>
   );
 }
---
+++ Web_Mobile/sekai/src/components/counter/index.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+const Counter = () => {
+    const [counterValue, setCounterValue] = useState(0)
+
+    return (
+        <div>
+            <div>{counterValue}</div>
+            <div>
+                <button onClick={() => setCounterValue(counterValue - 1) }>-</button>
+                <button onClick={() => setCounterValue(counterValue + 1) }>+</button>
+            </div>
+        </div>
+    )
+}
+
+export default Counter
