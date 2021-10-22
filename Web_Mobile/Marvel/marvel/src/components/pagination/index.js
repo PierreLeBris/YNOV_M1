97d5aa02369608b2ff96cf7c0b9cc9cf088cbeff
PierreLeBris <pierre.lebris@outlook.com>
2021-10-22 09:25:38 +0200

    oui

---
+++ Web_Mobile/Marvel/marvel/src/components/pagination/index.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+
+const pageCreator = (pageNumber,setCurrentPage) => {
+    let pageElement = []
+
+    for (let i = 0; i <= pageNumber; i++) {
+        pageElement.push(<p onClick={() => setCurrentPage(i)}>{i}</p>)
+    }
+    return pageElement
+}
+
+const Pagination = ({total, setCurrentPage, valueOffset}) => {
+    const [pages, setPages] = useState(0)
+    useEffect(() => {
+        const numberOfPages = total / valueOffset
+        setPages (Math.ceil(numberOfPages))
+    }, [total])
+
+    if (total === 0) return null ;
+
+    return (
+        <div>
+            {pageCreator(pages, setCurrentPage)}
+        </div>
+    )
+
+}
+
+export default Pagination;
--- Web_Mobile/Marvel/marvel/src/screens/home.js
+++ Web_Mobile/Marvel/marvel/src/screens/home.js
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
 import md5 from 'md5';
-
+import Pagination from '../Pagination';
 import privateKey from '../secret'
 
-const Home = props => {    
+const Home = props => { 
+    const valueOffset = 20
+    const [currentPage, setCurrentPage] = useState(0)
+    const [total, setTotal] = useState(0)
     const [charactersList, setCharactersList] = useState([])
     useEffect( () => {
         const publicKey = 'db535897f3ab7638b2faa88a557a45be';
@@ -19,18 +22,24 @@ const Home = props => {
         url: generateUrl,
         params: {
             ts: timestamp,
-            apiKey: publicKey,
+            apikey: publicKey,
             hash: hash
         }
       }).then(res => {
-          console.log("res.data.data.result",res.data.data.result)
-          setCharactersList(res.data.data.result)
+          console.log("res.data.data.results",res.data.data.results)
+          setCharactersList(res.data.data.results)
+          setTotal(res.data.data.total)
       }).catch(err => {
           console.log(err)
       })
-    }, [])
+    }, [currentPage])
     return <div>
-        {charactersList.map(character => <p>{character.name}</p>)}
+        {charactersList.map(character => <p>{character?.name}</p>)}
+        <Pagination
+        total={total}
+        setCurrentPage={setCurrentPage}
+        valueOffset={valueOffset}
+        ></Pagination>
     </div>
 };
 
