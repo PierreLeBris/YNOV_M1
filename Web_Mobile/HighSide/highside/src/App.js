34e522311530ee20595c11ff771a0f53a2020c15
PierreLeBris <pierre.lebris@outlook.com>
2021-10-22 11:22:54 +0200

    oui

--- Web_Mobile/HighSide/highside/src/App.js
+++ Web_Mobile/HighSide/highside/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import logo2 from './HighSide-anim-logo_@2x.gif'
 import './App.css';
+import SearchBar from './components/search';
+import Slide from './components/slide';
+
 
 function App() {
   return (
     <div className="App">
+    <img src={logo2}/>
       <header className="App-header">
+      <SearchBar></SearchBar>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +24,7 @@ function App() {
         >
           Learn React
         </a>
+        <Slide></Slide>
       </header>
     </div>
   );
