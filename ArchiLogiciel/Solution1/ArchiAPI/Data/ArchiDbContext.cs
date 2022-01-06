08ff98eace00d75498348eb72a8da193513c8e34
PierreLeBris <pierre.lebris@outlook.com>
2022-01-06 10:48:30 +0100

    oui

---
+++ ArchiLogiciel/Solution1/ArchiAPI/Data/ArchiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Archi.Api.Data
+{
+    public class ArchiDbContext: DbContext
+    {
+        public ArchiDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+    }
+}
