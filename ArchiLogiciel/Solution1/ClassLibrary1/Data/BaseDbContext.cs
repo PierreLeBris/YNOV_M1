40313cf670f8237ca0e5c02c6d077aeb99a507df
PierreLeBris <pierre.lebris@outlook.com>
2022-01-06 18:15:53 +0100

    oui

---
+++ ArchiLogiciel/Solution1/ClassLibrary1/Data/BaseDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Archi.Library.Data
+{
+    public class BaseDbContext : DbContext
+    {
+    }
+}
