d348ce544549004a77983dbc9da04de06a0458af
PierreLeBris <pierre.lebris@outlook.com>
2022-03-02 11:23:18 +0100

    oui

--- DevLogiciel/M1Zelda/src/model/Data.java
+++ DevLogiciel/M1Zelda/src/model/Data.java
@@ -5,13 +5,16 @@ import java.util.List;
 
 public class Data {
 
-    final static int MAPLENGTH = 20;
-    final static int MAPWIDTH = 20;
+    final static int MAPLENGTH= 20;
+    final static int MAPWIDTH = 15;
+    public final static int NBENEMIS = 10;
+    public final static int NBITEMS = 5;
 
     char[][] map;
     Element player = new Player(5,5,"Link", 'L');
     List<Enemy> enemies = new ArrayList<Enemy>();
     List<Item> items = new ArrayList<Item>();
+
 
 
     public Element getPlayer() {
@@ -48,6 +51,14 @@ public class Data {
 
     public static int getMapWidth(){
         return MAPWIDTH;
+    }
+
+    public static int getNbenemis() {
+        return NBENEMIS;
+    }
+
+    public static int getNbitems() {
+        return NBITEMS;
     }
 
     public List<Item> getItems() {
